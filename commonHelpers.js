73e4d4a2170c2197cfdd9610e485e98364ed01ab
AnnaVezhdel <avezhdel@gmail.com>
2024-08-18 16:44:37 +0000

    Deploying to gh-pages from @ AnnaVezhdel/test@0721251c4476f1592874fdae96d16a90371fb068 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{S as p}from"./assets/vendor-Bit1m2CU.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();new p(".swiper-projects",{direction:"horizontal",navigation:{nextEl:".projects-btn-next",prevEl:".projects-btn-prev"},slidesPerView:1,spaceBetween:34,keyboard:{enabled:!0,onlyInViewport:!1}});const a=document.querySelector(".projects-list"),n=[{mob_1x:"book-mob.jpg",mob_2x:"book-mob-2x.jpg",tab_1x:"book-tab.jpg",tab_2x:"book-tab-2x.jpg",main:"book-tab.jpg",description:"ReadJourney"},{mob_1x:"pet-mob.jpg",mob_2x:"pet-mob-2x.jpg",tab_1x:"pet-tab.jpg",tab_2x:"pet-tab-2x.jpg",main:"pet-tab.jpg",description:"PetLove"},{mob_1x:"vocab-mob.jpg",mob_2x:"vocab-mob-2x.jpg",tab_1x:"vocab-tab.jpg",tab_2x:"vocab-tab-2x.jpg",main:"vocab-tab.jpg",description:"VocabBuilder"}];function l(c,o,s,r,e,t){const i=`
+import{S as p}from"./assets/vendor-Bit1m2CU.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();new p(".swiper-projects",{direction:"horizontal",navigation:{nextEl:".projects-btn-next",prevEl:".projects-btn-prev"},slidesPerView:1,spaceBetween:34,keyboard:{enabled:!0,onlyInViewport:!1}});const a=document.querySelector(".projects-list"),n=[{mob_1x:"./img/book-mob.jpg",mob_2x:"./img/book-mob-2x.jpg",tab_1x:"./img/book-tab.jpg",tab_2x:"./img/book-tab-2x.jpg",main:"./img/book-tab.jpg",description:"ReadJourney"},{mob_1x:"./img/pet-mob.jpg",mob_2x:"./img/pet-mob-2x.jpg",tab_1x:"./img/pet-tab.jpg",tab_2x:"./img/pet-tab-2x.jpg",main:"../img/pet-tab.jpg",description:"PetLove"},{mob_1x:"../src/img/vocab-mob.jpg",mob_2x:"../src/img/vocab-mob-2x.jpg",tab_1x:"../src/img/vocab-tab.jpg",tab_2x:"../src/img/vocab-tab-2x.jpg",main:"../src/img/vocab-tab.jpg",description:"VocabBuilder"}];function l(c,o,s,r,e,t){const i=`
       <li class="swiper-slide projects-slide">
         <div class="projects-items">
           <div class="projects-container">
@@ -27,19 +27,19 @@ import{S as p}from"./assets/vendor-Bit1m2CU.js";(function(){const o=document.cre
             <picture class="projects-picture-item">
               <source
                 srcset="
-                ../src/img/${s} 1x,
-                ../src/img/${r} 2x"
+                ${s} 1x,
+                ${r} 2x"
                 media="(min-width: 768px)"
               />
               <source
                 srcset="
-                ../src/img/${c} 1x,
-                ../src/img/${o} 2x"
+                ${c} 1x,
+                ${o} 2x"
                 media="(max-width: 767px)"
               />
               <img
                 class="projects-img"
-                src="../src/img/${e}"
+                src="${e}"
                 alt="${t}"
                 width="614"
                 height="385"
