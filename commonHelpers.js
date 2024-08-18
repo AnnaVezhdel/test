c051bd3bd2b9c805ea3876e0a9feeb32bf9e8969
AnnaVezhdel <avezhdel@gmail.com>
2024-08-18 17:04:43 +0000

    Deploying to gh-pages from @ AnnaVezhdel/test@b8e01b94f7acde4441114adc64721c134845ec93 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{S as p}from"./assets/vendor-Bit1m2CU.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();new p(".swiper-projects",{direction:"horizontal",navigation:{nextEl:".projects-btn-next",prevEl:".projects-btn-prev"},slidesPerView:1,spaceBetween:34,keyboard:{enabled:!0,onlyInViewport:!1}});const a=document.querySelector(".projects-list"),n=[{mob_1x:"./img/book-mob.jpg",mob_2x:"./img/book-mob-2x.jpg",tab_1x:"./img/book-tab.jpg",tab_2x:"./img/book-tab-2x.jpg",main:"./img/book-tab.jpg",description:"ReadJourney"},{mob_1x:"./src/img/pet-mob.jpg",mob_2x:"./src/img/pet-mob-2x.jpg",tab_1x:"./src/img/pet-tab.jpg",tab_2x:"./src/img/pet-tab-2x.jpg",main:"../img/pet-tab.jpg",description:"PetLove"},{mob_1x:"../src/img/vocab-mob.jpg",mob_2x:"../src/img/vocab-mob-2x.jpg",tab_1x:"../src/img/vocab-tab.jpg",tab_2x:"../src/img/vocab-tab-2x.jpg",main:"../src/img/vocab-tab.jpg",description:"VocabBuilder"}];function l(c,o,s,r,e,t){const i=`
+import{S as l}from"./assets/vendor-Bit1m2CU.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const b="/test/assets/book-mob-BdhA58_h.jpg",d="/test/assets/book-mob-2x-j9KOIk62.jpg",a="/test/assets/book-tab--kIxrC0Y.jpg",m="/test/assets/book-tab-2x-D_63PazX.jpg",u="/test/assets/pet-mob-C1HkQvOk.jpg",j="/test/assets/pet-mob-2x-B5xWtdrT.jpg",n="/test/assets/pet-tab-CrhDqkQQ.jpg",x="/test/assets/pet-tab-2x-D-11kDrl.jpg",_="/test/assets/vocab-mob-CcDreXMM.jpg",f="/test/assets/vocab-mob-2x-BOoZoPUC.jpg",p="/test/assets/vocab-tab-Dsv7O7Av.jpg",g="/test/assets/vocab-tab-2x-CLu38__-.jpg";new l(".swiper-projects",{direction:"horizontal",navigation:{nextEl:".projects-btn-next",prevEl:".projects-btn-prev"},slidesPerView:1,spaceBetween:34,keyboard:{enabled:!0,onlyInViewport:!1}});const v=[{mob_1x:b,mob_2x:d,tab_1x:a,tab_2x:m,main:a,description:"ReadJourney"},{mob_1x:u,mob_2x:j,tab_1x:n,tab_2x:x,main:n,description:"PetLove"},{mob_1x:_,mob_2x:f,tab_1x:p,tab_2x:g,main:p,description:"VocabBuilder"}];new l(".swiper-projects",{direction:"horizontal",navigation:{nextEl:".projects-btn-next",prevEl:".projects-btn-prev"},slidesPerView:1,spaceBetween:34,keyboard:{enabled:!0,onlyInViewport:!1}});const h=document.querySelector(".projects-list");function y(i,s,r,o,e,t){const c=`
       <li class="swiper-slide projects-slide">
         <div class="projects-items">
           <div class="projects-container">
@@ -27,14 +27,14 @@ import{S as p}from"./assets/vendor-Bit1m2CU.js";(function(){const o=document.cre
             <picture class="projects-picture-item">
               <source
                 srcset="
-                ${s} 1x,
-                ${r} 2x"
+                ${r} 1x,
+                ${o} 2x"
                 media="(min-width: 768px)"
               />
               <source
                 srcset="
-                ${c} 1x,
-                ${o} 2x"
+                ${i} 1x,
+                ${s} 2x"
                 media="(max-width: 767px)"
               />
               <img
@@ -48,5 +48,5 @@ import{S as p}from"./assets/vendor-Bit1m2CU.js";(function(){const o=document.cre
           </div>
         </div>
       </li>
-    `;a.insertAdjacentHTML("beforeend",i)}n.map(({mob_1x:c,mob_2x:o,tab_1x:s,tab_2x:r,main:e,description:t})=>{l(c,o,s,r,e,t)});
+    `;h.insertAdjacentHTML("beforeend",c)}v.map(({mob_1x:i,mob_2x:s,tab_1x:r,tab_2x:o,main:e,description:t})=>{y(i,s,r,o,e,t)});
 //# sourceMappingURL=commonHelpers.js.map
